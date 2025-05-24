0e072da9b407ddb44147bf57fe778fd8676aa32d
PolinaKliushova <prekrasnayapolyachka@gmail.com>
2025-05-24 20:17:59 +0300

    add Django admin interface

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import User
 
-# Register your models here.
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'gender',
+        'first_name',
+        'last_name',
+        'phone',
+        'email',
+        'location',
+        'picture',
+    )
