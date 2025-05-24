83b075d6831231d1c69318d95466997554a2cdbf
PolinaKliushova <prekrasnayapolyachka@gmail.com>
2025-05-24 20:41:00 +0300

    add user_detail

--- users/views.py
+++ users/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from django.shortcuts import get_object_or_404
+from .models import User
+from .serializers import UserSerializer
 
-# Create your views here.
+@api_view(['GET'])
+def user_detail(request, user_id):
+    user = get_object_or_404(User, id=user_id)
+    return Response(UserSerializer(user).data)
