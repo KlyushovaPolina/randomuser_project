51cfe8ad152da44d2a011645034f16ac27cceaa6
PolinaKliushova <prekrasnayapolyachka@gmail.com>
2025-05-24 19:45:09 +0300

    create user model

--- users/models.py
+++ users/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    gender = models.CharField(max_length=10)
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    phone = models.CharField(max_length=30)
+    email = models.EmailField()
+    location = models.CharField(max_length=255)
+    picture = models.URLField()
