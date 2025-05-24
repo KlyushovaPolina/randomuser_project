9d981ae398fabe92816c94e4a3d05bfbf9bf3997
PolinaKliushova <prekrasnayapolyachka@gmail.com>
2025-05-24 20:43:05 +0300

    add random user api

--- users/urls.py
+++ users/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('<int:user_id>/', views.user_detail, name='user-detail'),
+    path('random/', views.random_user, name='random-user'),
 ]
--- users/views.py
+++ users/views.py
@@ -3,8 +3,18 @@ from rest_framework.decorators import api_view
 from django.shortcuts import get_object_or_404
 from .models import User
 from .serializers import UserSerializer
+import random
 
 @api_view(['GET'])
 def user_detail(request, user_id):
     user = get_object_or_404(User, id=user_id)
+    return Response(UserSerializer(user).data)
+
+@api_view(['GET'])
+def random_user(request):
+    count = User.objects.count()
+    if count == 0:
+        return Response({"error": "No users in DB"})
+    random_index = random.randint(0, count - 1)
+    user = User.objects.all()[random_index]
     return Response(UserSerializer(user).data)
